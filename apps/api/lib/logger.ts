c64a690452e251be5147fee356dbed4ab0901a94
Łukasz Myśliński <lukasz@mlnative.com>
2025-06-26 13:05:35 +0200

    chore: add env information to the logger

--- apps/api/lib/logger.ts
+++ apps/api/lib/logger.ts
@@ -1,14 +1,19 @@
 import pino from 'pino';
 import { envVars } from './env';
 
-export const logger = pino(
+export const rootLogger = pino(
   { level: 'info' },
   envVars.AXIOM_TOKEN ?
-  pino.transport({
-    target: '@axiomhq/pino',
-    options: {
-      dataset: envVars.AXIOM_DATASET,
-      token: envVars.AXIOM_TOKEN,
-    },
-  }) : undefined,
+    pino.transport({
+      target: '@axiomhq/pino',
+      options: {
+        dataset: envVars.AXIOM_DATASET,
+        token: envVars.AXIOM_TOKEN,
+      },
+    }) : undefined,
 );
+
+export const logger = rootLogger.child({
+  env: envVars.ENVIRONMENT,
+  baseUrl: envVars.BASE_URL,
+})
