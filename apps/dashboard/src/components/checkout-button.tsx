fb8168fe36ad01f00d919dafe780da2980f942f5
Lukasz Myslinski <lukasz@mlnative.com>
2025-10-26 20:53:43 +0100

    chore: crown icon

--- apps/dashboard/src/components/checkout-button.tsx
+++ apps/dashboard/src/components/checkout-button.tsx
@@ -1,4 +1,4 @@
-import { Star } from "lucide-react";
+import { Crown, Star } from "lucide-react";
 import { useAuthenticatedUser } from "@/hooks/use-user";
 import { authClient } from "@/lib/auth-client";
 import { Button } from "./ui/button";
@@ -7,6 +7,7 @@ export default function CheckoutButton() {
   const { session } = useAuthenticatedUser();
   return (
     <Button
+      className="cursor-pointer"
       onClick={async () => {
         await authClient.checkout({
           products: ["f772061e-7ef7-4628-b7e2-c7f9c2eb44a7"],
@@ -14,7 +15,7 @@ export default function CheckoutButton() {
         });
       }}
     >
-      <Star />
+      <Crown />
       Upgrade
     </Button>
   );
