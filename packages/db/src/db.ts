5a82341463f054d2ecb871680bd4149a8d6eb3ad
Lukasz Myslinski <lukasz@mlnative.com>
2025-12-12 22:00:28 +0100

    feat: move to neon

--- packages/db/src/db.ts
+++ packages/db/src/db.ts
@@ -1,8 +1,8 @@
-import { drizzle } from "drizzle-orm/postgres-js";
-import postgres from "postgres";
+import { neon } from "@neondatabase/serverless";
+import { drizzle } from "drizzle-orm/neon-http";
 import * as schema from "./schema";
 
-const queryClient = postgres(
+const queryClient = neon(
   Bun.env.DATABASE_URL ||
     (() => {
       throw new Error("DATABASE_URL is not defined");
